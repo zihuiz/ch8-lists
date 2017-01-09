2899b469fe23842e9bd8d3f9bd120c98b5ab1988
Joel Ross <joelross@uw.edu>
2017-01-08 20:32:14 -0800

    Typo fix

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -17,27 +17,27 @@ sentence <- tolower("I do not like green eggs and ham. I do not like them, Sam-I
 # Use the `strsplit()` function to split the sentence into vector of letters.
 # Hint: split on `""` to split on everything
 # Note: this will return a _list_ with 1 element (which is the vector of letters)
-letters.list <- strsplit(sentence,"")
+ltrs.list <- strsplit(sentence,"")
 
 # Extract the vector of letters from the resulting list
-letters <- letters.list[[1]]
+ltrs <- ltrs.list[[1]]
 
 # Use the `unique()` function to get a vector unique letters
-letters.unique <- unique(letters)
+ltrs.unique <- unique(ltrs)
 
 # Define a function CountOccurances that takes in two parameters: a letter and a vector letters.
 # The function should return how many times that letter occurs in the vector
 # Hint: use a filter!
-CountOccurances <- function(ltr, all.letters){
-  return(length(all.letters[all.letters == ltr]))
+CountOccurances <- function(ltr, all.ltrs){
+  return(length(all.ltrs[all.ltrs == ltr]))
 }
 
 # Call your CountOccurances() function to see how many times the letter 'e' is in your sentence.
-CountOccurances('e', letters)
+CountOccurances('e', ltrs)
 
 # Use `sapply()` to apply your CountOccurances() function to each unique letter in the vector to determine its frequency!
 # Convert the result into a list (using `as.list`).
-frequencies <- as.list(sapply(letters.unique, CountOccurances, letters))
+frequencies <- as.list(sapply(ltrs.unique, CountOccurances, ltrs))
 
 # Print the resulting list of frequencies
 print(frequencies)
