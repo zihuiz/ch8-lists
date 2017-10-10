81b39d0e2712fae12773e792fba67b481c4f865b
zihui zhang <zihuiz@uw.edu>
2017-10-10 15:09:04 -0700

    add my answers

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,24 +1,24 @@
 # Exercise 1: Lists
 
 # Create a vector of everything you ate for breakfast
-
+breakfast <- c("cheese", "hashbrown", "bacon", "egg")
 
 # Create a vector of everything you ate for lunch
-
+lunch <- c("cake", "chicken")
 
 # Create a list "meals" that has contains your `breakfast` and `lunch`
-
+meals <- list(breakfast=breakfast, lunch=lunch)
 
 # Add a "dinner" element to your "meals" list that has what you plan to eat for dinner
-
+meals[['dinner']] <- c("fish", "sandwich")
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-
+dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason)
-
+meals[[5]] <- meals[['lunch']]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early.meals`
@@ -27,10 +27,14 @@
 
 ### Bonus ###
 # Create a list that has the number of items you ate for each meal
-# Hint: use the `lappy()` function
-
+# Hint: use the `lapply()` function
+len <- lapply(meals, length)
 
 # Write a function that adds pizza to every meal
-
+AddPizza <- function(x){
+  results <- c(x, 'pizza')
+  return(results)
+}
 
 # Add pizza to every meal!
+better.meal <- lapply(meals, AddPizza)
